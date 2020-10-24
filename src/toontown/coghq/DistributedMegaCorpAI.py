91ad1c9553b4a8344b4bb18bf36047b842b418f1
Anonymous <10155689+AnonymousDeveloper65535@users.noreply.github.com>
2020-10-23 22:45:07 -0400

    Remove empire source and add anesidora source

--- src/toontown/coghq/DistributedMegaCorpAI.py
+++
@@ -1,8 +0,0 @@
-from direct.directnotify import DirectNotifyGlobal
-from . import DistributedFactoryAI
-
-class DistributedMegaCorpAI(DistributedFactoryAI.DistributedFactoryAI):
-    notify = DirectNotifyGlobal.directNotify.newCategory('DistributedBrutalFactoryAI')
-
-    def __init__(self, air, factoryId, zoneId, entranceId, avIds):
-        DistributedFactoryAI.DistributedFactoryAI.__init__(self, air, factoryId, zoneId, entranceId, avIds)
--- src/toontown/toon/DistributedNPCScientistAI.py
+++
@@ -1,9 +0,0 @@
-from . import DistributedNPCToonBaseAI
-import random
-
-Animation = random.choice(["ScientistPlay", "ScientistWork", "ScientistLessWork", "ScientistJealous"])
-
-class DistributedNPCScientistAI(DistributedNPCToonBaseAI.DistributedNPCToonBaseAI):
-
-    def getStartAnimState(self):
-        return 'ScientistEmcee' if self.npcId == 2020 else Animation
