9dfaade175cd777641d7b970d4216b45e98e59c8
Anonymous <10155689+AnonymousDeveloper65535@users.noreply.github.com>
2020-11-07 02:05:31 -0500

    DistributedBossbotBossAI: Fix darn disney's CODE
    XD

--- toontown/suit/DistributedBossbotBossAI.py
+++ toontown/suit/DistributedBossbotBossAI.py
@@ -1084,7 +1084,7 @@ class DistributedBossbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
                 toonLevel = toon.getNumPromotions(self.dept)
                 totalCogSuitLevels += toonLevel
                 totalNumToons += 1
-                if (toon.cogLevels > highestCogSuitLevel):
+                if (toon.cogLevels[0] > highestCogSuitLevel):
                     highestCogSuitLevel = toonLevel
 
         if not totalNumToons:
