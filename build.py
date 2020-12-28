f36261be96dea06d3f887eb64c91e9fbeb5ed46b
Anonymous <10155689+AnonymousDeveloper65535@users.noreply.github.com>
2020-12-27 20:28:28 -0500

    add build and main for production

---
+++ build.py
@@ -0,0 +1,20 @@
+import argparse
+import sys
+
+from pypperoni.cmake import CMakeFileGenerator
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--nthreads', '-t', type=int, default=4,
+                    help='Number of threads to use')
+args = parser.parse_args()
+
+c = CMakeFileGenerator('TTGGameEngine', outputdir='build', nthreads=args.nthreads)
+c.add_tree('toontown')
+c.add_tree('otp')
+c.add_tree('direct')
+c.add_tree('pandac')
+c.add_tree('pytz')
+c.add_tree('dependencies/panda/python/Lib/site-packages/MySQLdb')
+c.add_file('toontown/toonbase/ToontownStart.py')
+c.modules['toontown.toonbase.ToontownStart'].set_as_main()
+c.run()
--- main.py
+++ main.py
@@ -4,5 +4,44 @@ import sys
 
 
 
-os.environ["TTG_GAMESERVER"] = "TODO"
+os.environ["TTG_GAMESERVER"] = "localhost"
+# Original code: Toontown fellowship clientstartdist.
+
+# This is a temp patch.
+# It should really be done by the runtime (e.g. fellowship.exe):
+import sys
+sys.path = ['.']
+
+# Replace some modules that do exec:
+import collections
+collections.namedtuple = lambda *x: tuple
+
+# This is included in the package by the prepare_client script. It contains the
+# PRC file data, and (stripped) DC file:
+#import game_data TODO
+import builtins
+
+# Load all of the packaged PRC config page(s):
+from pandac.PandaModules import *
+#for i, config in enumerate(game_data.CONFIG):
+ #   name = 'GameData config page #' + str(i)
+  #  loadPrcFileData(name, config)
+
+# The VirtualFileSystem, which has already initialized, doesn't see the mount
+# directives in the config(s) yet. We have to force it to load them manually:
+vfs = VirtualFileSystem.getGlobalPtr()
+mounts = ConfigVariableList('vfs-mount')
+for mount in mounts:
+    mountFile, mountPoint = (mount.split(' ', 2) + [None, None, None])[:2]
+    mountFile = Filename(mountFile)
+    mountFile.makeAbsolute()
+    mountPoint = Filename(mountPoint)
+    vfs.mount(mountFile, mountPoint, 0)
+
+# Next, let's get the DC stream:
+#builtins.dcStream = StringStream(game_data.DC)
+
+# Finally, start the game:
+
 import toontown.toonbase.ToontownStart
+
