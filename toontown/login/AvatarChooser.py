9c1a0f47267faec5b83e2f74f2adf7615ca75dbf
Anonymous <mgracer48@yahoo.com>
2021-12-17 02:42:28 -0500

    General: Hacky fix for fixing widescreen issue
    For loading screen
    Pick a toon background needs to be adjusted by artists to work

--- toontown/login/AvatarChooser.py
+++ toontown/login/AvatarChooser.py
@@ -134,10 +134,12 @@ class AvatarChooser(StateData.StateData):
         gui = loader.loadModel("phase_3/models/gui/pick_a_toon_gui")
         gui2 = loader.loadModel("phase_3/models/gui/quit_button")
         newGui = loader.loadModel("phase_3/models/gui/tt_m_gui_pat_mainGui")
-        self.pickAToonBG = newGui.find("**/tt_t_gui_pat_background")
+        #self.pickAToonBG = newGui.find("**/tt_t_gui_pat_background")
+        self.pickAToonBG = OnscreenImage('phase_3/maps/tt_t_gui_pat_background.png')
         self.pickAToonBG.reparentTo(hidden)
         self.pickAToonBG.setPos(0.0, 2.73, 0.0)
         self.pickAToonBG.setScale(1, 1, 1)
+       # self.pickAToonBG.setScale(2,1,1)
         
         # set-up screen title
         self.title = OnscreenText(TTLocalizer.AvatarChooserPickAToon,
