ad447de055f7822473817e65d566c32bc3fb58cd
DarthNihilus1 <10155689+DarthNihilus1@users.noreply.github.com>
2022-01-06 21:34:05 -0500

    Update ToontownBattleGlobals.py
    Somehow this variable wasn’t modified

--- toontown/toonbase/ToontownBattleGlobals.py
+++ toontown/toonbase/ToontownBattleGlobals.py
@@ -14,7 +14,7 @@ BattleCamFaceOffPos = Point3(0, -10, 4)
 BattleCamDefaultPos = Point3(0, -8.6, 16.5)
 BattleCamDefaultHpr = Vec3(0, -61, 0)
 BattleCamDefaultFov = 100.0
-BattleCamMenuFov = 65.0
+BattleCamMenuFov = 95.0
 BattleCamJoinPos = Point3(0, -12, 13)
 BattleCamJoinHpr = Vec3(0, -45, 0)
 
