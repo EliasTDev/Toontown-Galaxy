6d1be02fd3d81cd78ab2d2282bccca660932c10a
Anonymous <mgracer48@yahoo.com>
2022-01-27 22:42:32 -0500

    Chat: Add current changes to whitelist nearby toons
    (Currently crashes the game)

--- otp/chat/ChatRouter.py
+++ otp/chat/ChatRouter.py
@@ -5,7 +5,12 @@ class ChatRouter(DistributedObjectGlobal):
     notify = directNotify.newCategory('ChatRouter')
 
     def sendChatMessage(self, message):
+        self.sendNearbyToons()
         self.sendUpdate('chatMessage', [message])
-
+        
     def sendWhisperMessage(self, message, receiverAvId):
         self.sendUpdate('whisperMessage', [message, receiverAvId])
+
+    def sendNearbyToons(self):
+        nearbyPlayers = base.localAvatar.getNearbyPlayers()
+        self.sendUpdate('nearbyToonsCommand', [nearbyPlayers])
